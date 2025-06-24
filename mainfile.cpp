3153afc2228e3b00294bfdc218d38e289c13ea7b
Navdeep Singh <navdeep24816@gmail.com>
2025-06-24 19:43:13 +0530

    Added new Features

--- mainfile.cpp
+++ mainfile.cpp
@@ -14,6 +14,7 @@ public:
         std::cin >> roll;
         std::cout << "Enter name: ";
         std::cin >> name;
+        // Assuming each subject is out of 100
         std::cout << "Enter English marks: ";
         std::cin >> English_marks;
         std::cout << "Enter Hindi marks: ";
@@ -24,12 +25,30 @@ public:
         std::cin >> Maths_marks;
         std::cout << "Enter Science marks: ";
         std::cin >> Science_marks;
+
     }
+
+    int persentage() {
+        float total_marks = English_marks + Hindi_marks + Chemistry_marks + Maths_marks + Science_marks;
+        return (total_marks / 500) * 100; 
+    }
+
+    char grade() {
+        int percent = persentage();
+        if (percent >= 90) return 'A';
+        else if (percent >= 80) return 'B';
+        else if (percent >= 70) return 'C';
+        else if (percent >= 60) return 'D';
+        else if (percent >= 50) return 'E';
+        else return 'F';
+    }
+
     void display() {
         std::cout << "Roll Number: " << roll << std::endl;
         std::cout << "Name: " << name << std::endl;
-        std::cout << "Total Marks: " << (English_marks + Hindi_marks + Chemistry_marks + Maths_marks + Science_marks) << std::endl;
+        std::cout << name << " has secured " << persentage() << "% and is graded " << grade() << std::endl;
     }
+
     
 };
 
